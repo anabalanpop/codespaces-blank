5c15c4d905e415054cce39fc7a955371351937ac
anabalanpop <123321783+anabalanpop@users.noreply.github.com>
2024-02-29 19:16:13 +0000

    first commit

---
+++ dist/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import weatherRoute from "./routes/weatherRoute.js";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+//our new route
+app.use("/api/weather", weatherRoute);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+//# sourceMappingURL=server.js.map
